d65165ee36bc745135a96474b89fa2e97ede4672
EmmanuelAdams <oluwaseunadams17@gmail.com>
2022-03-22 19:23:41 +0100

    deployment

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://adams_emmanuel:emmanuel2001@emmanuel.drqcj.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key',
+};
