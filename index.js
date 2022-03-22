4df75570971a5d3ec28c9937b7a8ffbd82c68572
EmmanuelAdams <oluwaseunadams17@gmail.com>
2022-03-22 07:23:31 +0100

    deployment fixes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const express = require('express');
 const { PubSub } = require('graphql-subscriptions');
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
@@ -8,13 +10,32 @@ const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req, pubsub }),
 });
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        'client',
+        'build',
+        'index.html'
+      )
+    )
+  );
+}
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
